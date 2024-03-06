2a36cef94d3e5c51e0e6b02d324799c335750095
Claudiano Alves <136628797+Claudiano-alves@users.noreply.github.com>
2024-03-05 21:25:35 -0300

    Add files via upload

---
+++ c#/EXEMPLOS/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Se a igual b bool isVerdadeiro recebe true, se nao false
+//bool isVerdadeiro = a == b ? true : false;
+
+Console.WriteLine("Digite a altura do retângulo: ");
+int altura = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite a largura do retângulo: ");
+int largura = Convert.ToInt32(Console.ReadLine());
+
+int area = altura * largura;
+Console.WriteLine($"A área: {area}");
