a87ac60f6c4afd831e544c07684ecdfd00e58a6b
Claudiano Alves <136628797+Claudiano-alves@users.noreply.github.com>
2024-03-21 21:31:14 -0300

    Adicionando aula 04

---
+++ php/Aula4/EstruturaControle2.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura de controle - Parte 2</title>
+</head>
+
+<body>
+
+    <?php 
+        // $soma = 0;
+        // $i = 0;
+        // while($i <= 9){
+            
+        // }
+        // echo "Soma: " . $soma;
+        
+        // do{
+            
+        //     $soma += $i;
+        //     $i++;
+        // }while($i <= 9);
+        // echo "Soma: " . $soma;
+        
+            
+        // $numeros = array (1,2,3,4,5,6,7,8,9,10);
+        // $media = 0;
+
+        // foreach($numeros as $soma){
+            
+        //     $media += $soma;
+        // }
+
+        // echo $media / sizeof($numeros) . "<br><br>";
+        // echo "Tipo de dado de media: " . gettype($media/sizeof($numeros));
+
+        // $estados = array ("SP"=>"São Paulo","RJ"=>"Rio de Janeiro","PE"=>"Recife","AM"=>"Manaus","GO"=>"Goiânia");
+
+        // foreach($estados as $index => $capital){
+        //     print("A capital de $index é $capital<br>");
+        // }
+
+
+    ?>
+</body>
+
+</html>
