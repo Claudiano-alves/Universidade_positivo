8e310cbed20fc61304d896adccd6ed0ba3ab0208
Auto Logon <autologon@CSED.LAB>
2024-04-09 21:04:24 -0300

    Inserida algumas bibliotecas

--- c#/ProjetoAPI/AppDbContext.cs
+++ c#/ProjetoAPI/AppDbContext.cs
@@ -5,6 +5,10 @@ class AppDbContext : DbContext
     public AppDbContext(AppDbContextOptions<AppDbContext> options)
         : base(options) { }
 
+    //Configuração do banco de dados MtSQL
+
+    protected override
+''
     //Classes que geram tabelas no banco de dados
     public DbSet<Tarefa> Tarefas => Set<Tarefas>();
     public DbSet<Livro> Livros => Set<Livro>();
